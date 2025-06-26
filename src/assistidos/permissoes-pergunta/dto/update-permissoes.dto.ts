08726cc2c4d21b4242805c4ce137d225e6b27573
Syllas <syllasbraga2@gmail.com>
2025-06-26 17:13:25 -0300

    Removendo arquivos desnecessários

--- src/assistidos/permissoes-pergunta/dto/update-permissoes.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePermissaoPerguntaDto } from './create-permissoes.dto';
-
-export class UpdatePermissaoPerguntaDto extends PartialType(CreatePermissaoPerguntaDto) {}
